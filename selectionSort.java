c2014b367bf9a716061b8da567f5dfb791c67b48
RashidSiddique12 <stylorashid786@gmail.com.com>
2022-10-21 10:14:43 +0530

    commit

--- selectionSort.java
+++ selectionSort.java
@@ -16,7 +16,7 @@ public class selectionSort {
     }
     public static void main(String[] args) {
         int arr[]={3,4,5,2,1};
-        secSort(arr, 5);
+        secSort(arr, 5); 
 
         for(int i=0; i<5; i++){
             System.out.print(arr[i]+" ");

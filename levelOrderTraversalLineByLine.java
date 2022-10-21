1e4bdf6254205fc2a8cc7c4a3845775d28987369
RashidSiddique12 <stylorashid786@gmail.com.com>
2022-10-21 09:52:31 +0530

    commit

---
+++ levelOrderTraversalLineByLine.java
@@ -0,0 +1,57 @@
+
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class Node  
+{ 
+    int key; 
+    Node left;
+    Node right;
+    Node (int k){
+        key = k;
+        left=right=null;
+    }
+  
+}
+
+
+public class levelOrderTraversalLineByLine { 
+    
+    public static void main(String args[]) 
+    { 
+        Node root=new Node(10);
+    	root.left=new Node(20);
+    	root.right=new Node(30);
+    	root.left.left=new Node(40);
+    	root.left.right=new Node(50);
+    	root.right.left=new Node(60);
+    	root.right.right=new Node(70);
+    	
+        printLevel(root);
+    } 
+    
+    public static void printLevel(Node root){
+        
+        if(root==null) return;
+        Queue <Node> q = new LinkedList <Node> ();
+        q.add(root);
+        q.add(null);
+        while(q.size() > 1){
+            Node curr = q.poll();
+            if(curr == null){
+                System.out.println();
+                q.add(null);
+                continue;
+            }
+            System.out.print(curr.key + " ");
+            if(curr.left != null){
+                q.add(curr.left);
+            }
+            if(curr.right != null){
+                q.add(curr.right);
+            }
+        }
+       
+    }   
+} 

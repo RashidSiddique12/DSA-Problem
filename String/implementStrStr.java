d6a17a17e05612193400bbc700daa67f46035c51
Md Rashid Siddique <94442631+RashidSiddique12@users.noreply.github.com>
2022-12-29 21:48:37 +0530

    first commit

---
+++ String/implementStrStr.java
@@ -0,0 +1,12 @@
+//https://practice.geeksforgeeks.org/batch/dsa-4/track/DSASP-Strings/problem/implement-strstr
+
+public class implementStrStr {
+
+     //Function to locate the occurrence of the string x in the string s.
+     int strstr(String s, String x)
+     {
+        // Your code here
+        return s.indexOf(x);
+     }
+    
+}

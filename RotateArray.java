cd6e79b479eadb63c63fe067367828ff2feb8a73
Md Rashid Siddique <94442631+RashidSiddique12@users.noreply.github.com>
2023-01-19 23:49:48 +0530

    leetcode189. Rotate Array

--- RotateArray.java
+++ RotateArray.java
@@ -3,23 +3,40 @@
 
 
 public class RotateArray {
-    
     public void rotate(int[] nums, int k) {
         k = k%nums.length; 
-        int arr[] = new int[nums.length];
-        int j=0;
-        for(int i=nums.length-k; i<nums.length; i++){
-            arr[j++] = nums[i];
-        }
+        // int arr[] = new int[nums.length];
+        // int j=0;
+        // for(int i=nums.length-k; i<nums.length; i++){
+        //     arr[j++] = nums[i];
+        // }
         
-        for(int i=0; i<nums.length-k; i++){
-            arr[j++] = nums[i];
-        }
+        // for(int i=0; i<nums.length-k; i++){
+        //     arr[j++] = nums[i];
+        // }
 
-        for(int i=0; i<nums.length; i++){
-            nums[i] = arr[i];
-        }
+        // for(int i=0; i<nums.length; i++){
+        //     nums[i] = arr[i];
+        // }
         
+        int len = nums.length;
+        reverse(nums,len-k, len-1); //567
+        reverse(nums, 0, len-1-k);
+        reverse(nums, 0, len-1);
+
+        
+
+
+        
+    }
+    public static void reverse(int []nums, int i, int j){
+        while(i<j){
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+            i++;
+            j--;
+        }
     }
     
 }

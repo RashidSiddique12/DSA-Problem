5e2ecb5827f5c58c62216d5e73430f202970a47e
Md Rashid Siddique <94442631+RashidSiddique12@users.noreply.github.com>
2023-01-26 22:05:26 +0530

    206. Reverse Linked List

---
+++ LeetCode 75days Challenge/ReverseLinkedList.java
@@ -0,0 +1,27 @@
+
+//https://leetcode.com/problems/reverse-linked-list/description/?envType=study-plan&id=level-1
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        if( head == null || head.next == null) return head;
+        ListNode curr = head;
+        ListNode prev = null;
+        ListNode next = head;
+        while(curr != null){
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+}

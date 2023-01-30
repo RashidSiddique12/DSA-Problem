a50f150000d18cfe169bfcd7790b7f9571ae36fc
Md Rashid Siddique <94442631+RashidSiddique12@users.noreply.github.com>
2023-01-30 23:15:00 +0530

    704. Binary Search

---
+++ LeetCode 75days Challenge/BinarySearch.java
@@ -0,0 +1,20 @@
+//https://leetcode.com/problems/binary-search/description/?envType=study-plan&id=level-1
+
+class Solution {
+    public int search(int[] nums, int target) {
+        
+        int low = 0;
+        int high = nums.length -1;
+        while(low <= high){
+            int mid = (low+(high))/2;
+            if(nums[mid] == target) return mid;
+            else if(nums[mid] < target){
+                low = mid + 1;
+            }
+            else{
+                high = mid - 1;
+            }
+        }
+        return -1;
+    }
+}

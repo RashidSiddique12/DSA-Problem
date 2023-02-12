0434377da4b984783abcb35e177118211260e9c5
Md Rashid Siddique <94442631+RashidSiddique12@users.noreply.github.com>
2023-02-12 22:50:12 +0530

    35. Search Insert Position

---
+++ Algorithm/SearchInsertPosition.java
@@ -0,0 +1,20 @@
+//https://leetcode.com/problems/search-insert-position/description/
+
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int l=0, h=nums.length - 1;
+        while(l <= h){
+            int mid = l + (h-l)/2;
+            if(nums[mid] == target ){
+                return mid;
+            }
+            if(nums[mid] < target){
+                l = mid + 1;
+            }
+            else{
+                h = mid - 1;
+            }
+        }
+        return l;
+    }
+}

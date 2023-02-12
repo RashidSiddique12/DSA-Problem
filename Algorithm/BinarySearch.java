7359ba36217de66c0ca6f1310a310c2db38aa9c9
Md Rashid Siddique <94442631+RashidSiddique12@users.noreply.github.com>
2023-02-12 22:51:11 +0530

    704. Binary Search

---
+++ Algorithm/BinarySearch.java
@@ -0,0 +1,20 @@
+//https://leetcode.com/problems/binary-search/description/?envType=study-plan&id=algorithm-i
+
+class Solution {
+    public int search(int[] nums, int target) {
+        int l=0;
+        int h = nums.length -1;
+        while(l <= h){
+            int mid = l + (h-l)/2;
+            if(nums[mid] == target) return mid;
+            else if(nums[mid] < target) {
+                l = mid +1;
+            }
+            else{
+                h = mid -1;
+            }
+        }
+        return -1;
+        
+    }
+}

bcb33f6e66bbddf0fdc7285c5a96c88d9e92c94d
Md Rashid Siddique <94442631+RashidSiddique12@users.noreply.github.com>
2023-03-06 23:07:33 +0530

    74. Search a 2D Matrix

---
+++ SearchA2DMatrix.java
@@ -0,0 +1,24 @@
+//https://leetcode.com/problems/search-a-2d-matrix/
+
+
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int r = matrix.length;
+        int c = matrix[0].length;
+        int i=0;
+        int j = c - 1;
+        while(i < r && j>=0){
+            if(matrix[i][j] == target)
+                return true;
+            else if(matrix[i][j] > target){
+                j--;
+            }
+            else{
+                i++;
+            }
+
+        }
+        return false;
+        
+    }
+}

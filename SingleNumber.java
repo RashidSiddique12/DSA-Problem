c23eb158d8e86767cf2da4c823d325d729719ead
Md Rashid Siddique <94442631+RashidSiddique12@users.noreply.github.com>
2023-03-25 20:22:26 +0530

    136. Single Number

--- SingleNumber.java
+++ SingleNumber.java
@@ -1,36 +1,46 @@
 
 //https://leetcode.com/problems/single-number/description/
 import java.util.*;
-public class SingleNumber {
-    
+class Solution {
     public int singleNumber(int[] nums) {
-        Map <Integer, Integer> hm = new HashMap<Integer, Integer> ();
-        for(int i=0; i<nums.length; i++){
-            if(hm.containsKey(nums[i])){
-                hm.put(nums[i], hm.get(nums[i])+1);
-            }
-            else{
-                hm.put(nums[i], 1);
+        // Map <Integer, Integer> hm = new HashMap<Integer, Integer> ();
+        // for(int i=0; i<nums.length; i++){
+        //     if(hm.containsKey(nums[i])){
+        //         hm.put(nums[i], hm.get(nums[i])+1);
+        //     }
+        //     else{
+        //         hm.put(nums[i], 1);
+        //     }
+        // }
+
+        // for(Map.Entry<Integer, Integer> e : hm.entrySet()){
+        //     if(e.getValue() == 1){
+        //         return e.getKey();
+        //     }
+        // }
+
+    // using Arrays.sort()
+        if(nums.length == 1){return nums[0];}
+        Arrays.sort(nums);
+        for(int i=0; i<nums.length-1; i = i+2){
+            if(nums[i] != nums[i+1]){
+                return nums[i];
             }
         }
-
-        for(Map.Entry<Integer, Integer> e : hm.entrySet()){
-            if(e.getValue() == 1){
-                return e.getKey();
-            }
-        }
-
+        if(nums[nums.length - 1] != nums[nums.length -2]) {return nums[nums.length - 1];}
         return -1;
 
-//         class Solution {
-//         public int singleNumber(int[] nums) {
-//         int ans=0;
-//         for(int i=0;i<nums.length;i++)
-//             ans=ans^nums[i]; // 0^2 = 2^2 = 0^1 =1
-//         return ans;          //0^4 = 4^1^2^1 = 4^2^2 = 4
-//         }
-//       }
 
         
     }
 }
+
+// using XOR (Exclusive or) Mthod
+    //     class Solution {
+    //     public int singleNumber(int[] nums) {
+    //     int ans=0;
+    //     for(int i=0;i<nums.length;i++)
+    //         ans=ans^nums[i]; // 0^2 = 2^2 = 0^1 =1
+    //     return ans;          //0^4 = 4^1^2^1 = 4^2^2 = 4
+    //     }
+    //   }

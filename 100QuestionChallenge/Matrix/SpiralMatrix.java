3ea4428fe2bd48c64ac1f90125d9dceba0dc5425
Md Rashid Siddique <94442631+RashidSiddique12@users.noreply.github.com>
2023-07-27 23:59:31 +0530

    Add files via upload

--- 100QuestionChallenge/Matrix/SpiralMatrix.java
+++ 100QuestionChallenge/Matrix/SpiralMatrix.java
@@ -1,8 +1,41 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class SpiralMatrix {
 
-    public static List<Integer> spIntegers(int [][] matrix)
+    public static List<Integer> sprialMatrix(int[][] matrix) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+        int top = 0;
+        int bottom = n - 1;
+        int left = 0;
+        int right = m - 1;
+        List<Integer> list = new ArrayList<>();
+        while (left <= right && top <= bottom) {
+            for (int i = left; i <= right; i++) {
+                list.add(matrix[top][i]);
+            }
+            top++;
+            for (int i = top; i <= bottom; i++) {
+                list.add(matrix[i][right]);
+            }
+            right--;
+
+            if (top <= bottom) {
+                for (int i = right; i >= left; i--) {
+                    list.add(matrix[bottom][i]);
+                }
+                bottom--;
+            }
+            if (left <= right) {
+                for (int i = bottom; i >= top; i--) {
+                    list.add(matrix[i][left]);
+                }
+                left++;
+            }
+        }
+        return list;
+    }
 
     public static void main(String[] args) {
         int[][] matrix = { { 1, 2, 3, 4 }, { 5, 6, 7, 8 }, { 9, 10, 11, 12 } };

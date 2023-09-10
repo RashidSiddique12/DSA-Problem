076b1e0eb05079329b35fe7ceee38783bfe63f34
Md Rashid Siddique <94442631+RashidSiddique12@users.noreply.github.com>
2023-09-10 23:56:44 +0530

    rough

---
+++ 100QuestionChallenge/String/rough.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class rough {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a Integer : ");
+        int a = sc.nextInt();
+        System.out.println("Integer : " + a);
+        System.out.println("Enter Character : ");
+        char ch = sc.next().charAt(0);
+        System.out.println("character : " + ch);
+        System.out.println("Entre a word : ");
+        String word = sc.next();
+        System.out.println("Word : " + word);
+        sc.nextLine();
+        System.out.println("Entre a Sentence : ");
+        String str = sc.nextLine();
+        System.out.println("Sentence : " + str);
+        System.out.println("Entre a Integer : ");
+        int b = sc.nextInt();
+        System.out.println("Integer : " + b);
+
+    }
+
+}
